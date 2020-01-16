83e247d3ea590db957d8d85f0435edea6ba694b5
Zhen <zhangzhengbaiw@gmail.com>
2020-01-16 14:16:52 +0800

    fix: change server from http to express

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
-import http from 'http';
 import appFactory from './app';
 
 const app = appFactory();
 
 const PORT = 3000;
 
-const httpServer = http.createServer(app);
-httpServer.listen(PORT);
+app.listen(PORT);
